6d3797c9fa83e35d59381b9ae4dd9341b6c2567b
Thanh Nguyen <71760329+Thanhibanani@users.noreply.github.com>
2022-10-24 14:35:07 +0200

    Oppgave 1 og 2 passed

--- src/main/java/no/oslomet/cs/algdat/Oblig3/SBinTre.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig3/SBinTre.java
@@ -3,6 +3,7 @@ package no.oslomet.cs.algdat.Oblig3;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.Objects;
 import java.util.StringJoiner;
 
 public class SBinTre<T> {
@@ -83,31 +84,162 @@ public class SBinTre<T> {
     }
 
     public boolean leggInn(T verdi) {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
-    }
+        Node<T> p = rot, q= null; // p som starter i rot.
+        int cmp = 0; //hjelpe variabel
 
+        while (p !=null){
+            q=p; //q blir foreldre til p
+            cmp =comp.compare(verdi,p.verdi); //bruker komperator
+
+            p=cmp <0 ? p.venstre: p.høyre; //flytter p
+        }
+
+        //p er null, ute av treet. q er siste som er passert.
+        p=new Node<>(verdi,q); //Oppretter ny node.
+
+        if(q==null){
+            rot=p;
+        } else if(cmp<0) {
+            q.venstre=p;
+        }else{
+            q.høyre=p;
+            }
+        antall++;
+        return true;
+
+    }
     public boolean fjern(T verdi) {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+
+        if(verdi==null) return false; //treet har ingen nullverdier
+        Node<T> p =rot, q=null; //q skal være forelder til p
+
+       while(p !=null){
+           int cmp = comp.compare(verdi,p.verdi); //sammenligner
+           if(cmp<0){
+               q=p; p=p.venstre; //venstre
+           }else if(cmp>0){
+               q=p; p=p.høyre;//høyre
+           }
+           else break; //verdien ligger i p
+       }
+
+       if(p==null) return false; //finner ikke verdi
+
+        if(p.venstre ==null || p.høyre == null){
+            Node<T> b=p.venstre !=null ? p.venstre : p.høyre; //b er barnet//hvis noden har 0 eller 1 barn
+            if(p==rot){
+                rot=b;
+            } else if (p==q.venstre){
+                q.venstre = b;
+                if(b !=null){
+                    b.forelder =q;
+                }
+            }
+            else{
+                q.høyre =b;
+            if(b !=null){
+                b.forelder =q;
+            }
+            }
+        }
+        else  {
+            Node<T> s=p, r = p.høyre;
+
+            while(r.venstre !=null){
+                s=r;
+                r=r.venstre;
+            }
+
+            p.verdi = r.verdi;
+
+            if(s != p){
+                s.venstre =r.høyre;
+            }else {
+                s.høyre = r.høyre;
+            }
+            if(r.høyre !=null){
+                r.høyre.forelder =s;
+            }
+
+
+        }
+
+        antall--;
+        endringer++;
+        return true;
+
     }
 
     public int fjernAlle(T verdi) {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+         if(tom()){
+             return 0;
+         }
+
+         int verdiAntall=0;
+
+         while(fjern(verdi)){
+             verdiAntall++;
+         }
+         return verdiAntall;
     }
 
     public int antall(T verdi) {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+        Node<T> p = rot;
+        int antValue = 0;
+
+        while(p != null){
+            int cmp = comp.compare(verdi,p.verdi);
+            if(cmp<0){
+                p=p.venstre;
+            }else{
+                if(cmp==0) antValue++;
+                p=p.høyre;
+            }
+        }
+        return antValue;
     }
 
     public void nullstill() {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+        if(antall==0){
+            return;
+        }
+
+        Node<T> p = rot;
+
+        int stop =antall;
+
+        p= førstePostorden(p);
+
+        while(stop !=0;){
+            if(p != null){
+                fjern(p.verdi);
+            }
+            if(p !=null){
+                p.verdi=null;
+            }
+            if(p != null){
+                p =nestePostorden(p);
+            }
+            stop--;
+        }
+
     }
 
     private static <T> Node<T> førstePostorden(Node<T> p) {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+        Objects.requireNonNull(p);
+        while(true){
+            if(p.venstre != null){
+                p =p.venstre;
+            }else if(p.høyre !=null){
+                p=p.høyre;
+            }else{
+                return p;
+            }
+        }
     }
 
     private static <T> Node<T> nestePostorden(Node<T> p) {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+
     }
 
     public void postorden(Oppgave<? super T> oppgave) {
